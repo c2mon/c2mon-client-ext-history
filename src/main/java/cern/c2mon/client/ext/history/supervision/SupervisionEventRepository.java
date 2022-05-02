6bd541e1c4b87458b1b2a4b0976674a23d736a2d
tmolivei <tiago.marques.oliveira@cern.ch>
2022-05-02 17:17:51 +0200

    Update Supervision Repo

--- src/main/java/cern/c2mon/client/ext/history/supervision/SupervisionEventRepository.java
+++ src/main/java/cern/c2mon/client/ext/history/supervision/SupervisionEventRepository.java
@@ -1,13 +1,21 @@
 package cern.c2mon.client.ext.history.supervision;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author Manuel Bouzas Reguera
  */
 public interface SupervisionEventRepository extends JpaRepository<ServerSupervisionEvent, Long>{
-    List<ServerSupervisionEvent> findByIdAndEventTimeBetween(Long id, Date from, Date to);
+
+    List<ServerSupervisionEvent> findAllDistinctByIdAndEventTimeBetween(Long id, LocalDateTime from, LocalDateTime to);
+
+    List<ServerSupervisionEvent> findAllDistinctByIdAndEventTimeBetweenOrderByEventTimeDesc(Long id, LocalDateTime startTime, LocalDateTime endTime);
+
+    Page<ServerSupervisionEvent> findAllDistinctByIdOrderByEventTimeDesc(Long id, Pageable pageable);
+
 }
