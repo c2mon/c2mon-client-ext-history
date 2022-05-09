369c62c6ef28dfd0e12936f277458c838b5d61a4
tmolivei <tiago.marques.oliveira@cern.ch>
2022-05-09 08:58:47 +0200

    Add Laser specific Repos

---
+++ src/main/java/cern/c2mon/client/ext/history/laser/LaserAlarmUserConfig.java
@@ -0,0 +1,89 @@
+package cern.c2mon.client.ext.history.laser;
+
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.Type;
+import org.springframework.context.annotation.Profile;
+
+@Profile("enableLaser")
+@Entity
+@Data
+@Table(name = "lsr_alarm_user_config_v")
+public class LaserAlarmUserConfig {
+
+    @Id
+    @Column(name = "alarm_id")
+    private Long alarmId;
+
+    @Column(name = "fault_family")
+    private String faultFamily;
+
+    @Column(name = "fault_member")
+    private String faultMember;
+
+    @Column(name = "fault_code")
+    private String faultCode;
+
+    @Column(name = "system_name")
+    private String systemName;
+
+    @Column(name = "identifier")
+    private String identifier;
+
+    @Column(name = "problem_description")
+    private String problemDescription;
+
+    @Column(name = "cause")
+    private String cause;
+
+    @Column(name = "action")
+    private String action;
+
+    @Column(name = "consequence")
+    private String consequence;
+
+    @Column(name = "priority")
+    private Integer priority;
+
+    @Column(name = "help_url")
+    private String helpUrl;
+
+    @Column(name = "responsible_id")
+    private Long responsibleId;
+
+    @Column(name = "source_id")
+    private String sourceId;
+
+    @Column(name = "itn")
+    private String itn;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "enabled")
+    @Type(type="yes_no")
+    private Boolean enabled;
+
+    @Column(name = "ppm")
+    @Type(type="yes_no")
+    private Boolean ppm;
+
+    @Column(name = "accelerator")
+    private String accelerator;
+
+    @Column(name = "device_type")
+    private String deviceType;
+
+    @Column(name = "last_updated")
+    private LocalDateTime lastUpdated;
+
+    @Column(name = "config_id")
+    private Long configId;
+}
