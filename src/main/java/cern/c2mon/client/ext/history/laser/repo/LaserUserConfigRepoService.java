f7da76616a4ab4c9d77064fd853f47208b4c6297
tmolivei <tiago.marques.oliveira@cern.ch>
2022-05-11 11:11:37 +0200

    Update laser components

--- src/main/java/cern/c2mon/client/ext/history/laser/repo/LaserUserConfigRepoService.java
+++ src/main/java/cern/c2mon/client/ext/history/laser/repo/LaserUserConfigRepoService.java
@@ -7,11 +7,16 @@ import java.util.Optional;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 @Profile("enableLaser")
 @Service
 public interface LaserUserConfigRepoService extends JpaRepository<LaserUserConfig, Long> {
+
+    @Query("select luc from LaserUserConfig luc where luc.configName = :configName")
+    List<LaserUserConfig> findAllByConfigName(@Param("configName") String configName);
 
     Optional<LaserUserConfig> findByConfigName(String configName);
 
