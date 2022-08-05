fba516cc687a0ca5cd37c67ddb012850ff8e63da
ihovind <ingebrigt.hovind@cern.ch>
2022-08-05 15:49:35 +0200

    renaming

---
+++ src/main/java/cern/c2mon/client/ext/history/laser/repo/ShortTermLogHistoryService.java
@@ -0,0 +1,31 @@
+package cern.c2mon.client.ext.history.laser.repo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import cern.c2mon.client.ext.history.laser.ShortTermLog;
+
+public interface ShortTermLogHistoryService extends JpaRepository<ShortTermLog, Long> {
+
+  // step three pagination
+  @Query("SELECT DISTINCT a FROM ShortTermLog a WHERE "
+          + "a.tagServerTime BETWEEN :startTime AND :endTime")
+  Page<ShortTermLog> findAllByTagTimeBetween(
+          @Param("startTime") LocalDateTime from,
+          @Param("endTime") LocalDateTime to,
+          Pageable pageable);
+
+  // step one unique tags
+
+  @Query("SELECT DISTINCT a.id FROM ShortTermLog a WHERE "
+          + "a.tagServerTime BETWEEN :startTime AND :endTime ")
+  List<Long> findDistinctIdByTagTimeBetween(
+          @Param("startTime") LocalDateTime startTime,
+          @Param("endTime") LocalDateTime endTime);
+}
