ef8d1745a6b4b4d3728052e10378b16da0da88e2
ihovind <ingebrigt.hovind@cern.ch>
2022-08-12 14:40:55 +0200

    necessary changes for c2mon es publisher

--- src/main/java/cern/c2mon/client/ext/history/laser/repo/ShortTermLogHistoryService.java
+++ src/main/java/cern/c2mon/client/ext/history/laser/repo/ShortTermLogHistoryService.java
@@ -10,13 +10,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import cern.c2mon.client.ext.history.laser.ShortTermLog;
+import cern.c2mon.client.ext.history.laser.Shorttermlog;
 
 public interface ShortTermLogHistoryService extends JpaRepository<ShortTermLog, Long> {
 
   // step three pagination
   @Query("SELECT DISTINCT a FROM ShortTermLog a WHERE "
-          + "a.tagServerTime BETWEEN :startTime AND :endTime")
-  Page<ShortTermLog> findAllByTagTimeBetween(
+          + "a.tagServerTime BETWEEN :startTime AND :endTime "
+          + "ORDER BY a.tagServerTime ASC")
+  Page<ShortTermLog> findAllDistinctByTagServerTimeBetweenOrderByTagServerTimeDesc(
           @Param("startTime") LocalDateTime from,
           @Param("endTime") LocalDateTime to,
           Pageable pageable);
